1cbf2c341cbc4bc7c78ba457d7578ddd5e5bca5c
radzikdev <radzikdev@gmail.com>
2020-12-09 16:35:09 +0100

    Initial commit

---
+++ src/main/java/com/radzikdev/SpringWebService/SpringWebServiceApplication.java
@@ -0,0 +1,22 @@
+package com.radzikdev.SpringWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebServiceApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/test/java/com/radzikdev/SpringWebService/SpringWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radzikdev.SpringWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
